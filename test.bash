95bf415ebfc7a4210b24d9aafd4d9f12920cb667
Orechan <ryota@LAPTOP-PH37ODTE.localdomain>
2025-12-01 01:52:17 +0900

    test.bash変更

--- test.bash
+++ test.bash
@@ -1,23 +1,33 @@
 #!/bin/bash -xv
 # SPDX-FileCopyrightText: 2025 Ryota Miyauchi
+# SPDX-License-Identifier: BSD-3-Clause
 
-ng(){
-	echo ${1}行目が違うよ
-        res=1
+ng () {
+	echo "${1}行目のテストが失敗しました。"
+	res=1
 }
 
 res=0
 
-out=$(seq 5 | ./plus)
-[ "${out}" = 15.0] || ng "$LINENO"
+out=$(echo "180 degree" | ./convert)
+if [ "$?" -ne 0 ]; then
+	ng "$LINENO"
+fi
+echo "${out}" | grep "3.14" || ng "$LINENO"
 
-out=$(echo あ | ./ plus)
+out=$(echo "3.141592653589793 rad" | ./convert)
+if [ "$?" -ne 0 ]; then
+	ng "$LINENO"
+fi
+echo "${out}" | grep "180.0" || ng "$LINENO"
+
+echo "100" | ./convert > /dev/null 2>&1
 [ "$?" = 1 ] || ng "$LINENO"
-[ "${out}" = "" ] || ng "$LINENO"
 
-out=$(echo | ./plus)
-[ "$?" = 1 ] || ng "LINENO"
-[ "$?{out}" = "" ] || ng "$LINENO"
+echo "あ degree" | ./convert > /dev/null 2>&1
+[ "$?" = 1 ] || ng "$LINENO"
 
-[ "$res" = 0 ] && echo OK
+if [ "${res}" = 0 ]; then
+	echo OK
+fi
 exit $res
