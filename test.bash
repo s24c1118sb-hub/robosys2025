b137801de29cc5eb49515d2cc3cfdad5cec2a62a
Orechan <ryota@LAPTOP-PH37ODTE.localdomain>
2025-12-25 23:56:44 +0900

    correction

--- test.bash
+++ test.bash
@@ -10,16 +10,30 @@ ng () {
 res=0
 
 out=$(echo "180 degree" | ./convert)
-if [ "$?" -ne 0 ]; then
-	ng "$LINENO"
-fi
+if [ "$?" -ne 0 ]; then ng "$LINENO"; fi
 echo "${out}" | grep "3.14" || ng "$LINENO"
 
 out=$(echo "3.141592653589793 rad" | ./convert)
-if [ "$?" -ne 0 ]; then
-	ng "$LINENO"
-fi
+if [ "$?" -ne 0 ]; then ng "$LINENO"; fi
 echo "${out}" | grep "180.0" || ng "$LINENO"
+
+out=$(echo "0 degree" | ./convert)
+if [ "$?" -ne 0 ]; then ng "$LINENO"; fi
+echo "${out}" | grep "0.0" || ng "$LINENO"
+
+out=$(echo "-180 degree" | ./convert)
+if [ "$?" -ne 0 ]; then ng "$LINENO"; fi
+echo "${out}" | grep "\-3.14" || ng "$LINENO"
+
+out=$(echo "90     degree" | ./convert)
+if [ "$?" -ne 0 ]; then ng "$LINENO"; fi
+echo "${out}" | grep "1.57" || ng "$LINENO"
+
+out=$(printf "180 degree\n0 degree\n90 degree" | ./convert)
+line_count=$(echo "${out}" | wc -l)
+[ "${line_count}" -ge 3 ] || ng "$LINENO"
+
+
 
 echo "100" | ./convert > /dev/null 2>&1
 [ "$?" = 1 ] || ng "$LINENO"
@@ -27,7 +41,8 @@ echo "100" | ./convert > /dev/null 2>&1
 echo "あ degree" | ./convert > /dev/null 2>&1
 [ "$?" = 1 ] || ng "$LINENO"
 
-if [ "${res}" = 0 ]; then
-	echo OK
-fi
+echo "100 meter" | ./convert > /dev/null 2>&1
+[ "$?" = 1 ] || ng "$LINENO"
+
+[ "${res}" = 0 ] && echo OK
 exit $res
